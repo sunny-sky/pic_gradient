e26f38feff8332b8994605544123caa20005038d
helong <44635029+helong920@users.noreply.github.com>
2018-11-21 21:27:32 +0800

    添加删除

--- src/main/java/com/xjtu/pic_gradient/config/WebAppConfig.java
+++ src/main/java/com/xjtu/pic_gradient/config/WebAppConfig.java
@@ -3,6 +3,7 @@ package com.xjtu.pic_gradient.config;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.MultipartConfigElement;
@@ -27,5 +28,14 @@ public class WebAppConfig implements WebMvcConfigurer {
         factory.setMaxRequestSize("20MB");
         return factory.createMultipartConfig();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        //文件磁盘图片url 映射
+        //配置server虚拟路径，handler为前台访问的目录，locations为files相对应的本地路径
+        registry.addResourceHandler("/images/**").addResourceLocations("file:E:\\IDEA\\IntelliJ IDEA 2018.2\\workspace\\pic_gradient\\src\\main\\resources\\static\\images\\");
+    }
+
 }
 
--- src/main/java/com/xjtu/pic_gradient/controller/HelloController.java
+++ src/main/java/com/xjtu/pic_gradient/controller/HelloController.java
@@ -17,20 +17,36 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @MapperScan(value="com.xjtu.pic_gradient.mapper")
 public class HelloController {
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private PhotoMapper photoMapper;
-
     @RequestMapping("/index")
-    public String index2(@RequestParam(value = "name", defaultValue = "photo1") String name, Map<String, String> map) {
+    public String index2(@RequestParam(value = "name", defaultValue = "photo1") String name, Map<String, Object> map) {
+
+        List<String> photoNames = photoMapper.getName();
         map.put("albumName", name);
+        map.put("photoNames",photoNames);
+        System.out.println(photoNames.toString());
         return "index";
     }
+
+    @RequestMapping("/delete")
+    public String delete(@RequestParam(value = "name") String name, Map<String, Object> map) {
+        photoMapper.deleteName(name);
+        List<String> photoNames = photoMapper.getName();
+        String albumName = photoNames.get(0);
+        map.put("albumName", albumName);
+        map.put("photoNames",photoNames);
+        System.out.println(photoNames.toString());
+        return "index";
+    }
+
 
     @ResponseBody
     @RequestMapping("/hello")
@@ -46,13 +62,16 @@ public class HelloController {
     }
 
 
-
     @PostMapping("/uploadImg")
     public String uploadImg(@RequestParam("img1") MultipartFile multipartFile1,
                             @RequestParam("img2") MultipartFile multipartFile2,
                             @RequestParam("name") String name) throws IOException, InterruptedException {
-        String path = "D:\\workspace\\idea\\pic_gradient\\src\\main\\resources\\static\\images\\" + name;
+        String path = "E:\\IDEA\\IntelliJ IDEA 2018.2\\workspace\\pic_gradient\\src\\main\\resources\\static\\images\\" + name;
         System.out.println(path);
+
+        photoMapper.insertPhotoName(name);
+        System.out.println("插入成功！");
+
         SaveImg.saveImg(multipartFile1, path, "00.bmp");
         SaveImg.saveImg(multipartFile2, path, "64.bmp");
         while (!isExist(path)) {
@@ -69,4 +88,32 @@ public class HelloController {
         File file2 = new File(path + "\\64.bmp");
         return file1.exists() && file2.exists();
     }
+
+//    @PostMapping("/uploadImg")
+//    public String uploadImg(@RequestParam("img1") MultipartFile multipartFile1,
+//                            @RequestParam("img2") MultipartFile multipartFile2,
+//                            @RequestParam("name") String name) throws IOException, InterruptedException {
+//        /*String path = "D:\\workspace\\idea\\pic_gradient\\src\\main\\resources\\static\\images\\" + name;*/
+//        String path = "E:\\IDEA\\IntelliJ IDEA 2018.2\\workspace\\pic_gradient\\src\\main\\resources\\static\\images\\"+name;
+//        System.out.println(path);
+//
+//        photoMapper.insertPhotoName(name);
+//        System.out.println("插入成功！");
+//
+//        SaveImg.saveImg(multipartFile1, path, "00.bmp");
+//        SaveImg.saveImg(multipartFile2, path, "64.bmp");
+//        while (!isExist(path)) {
+//            System.out.println("图片未保存");
+//        }
+//        System.out.println("保存成功");
+//
+//        JNITest.generateImg(path);
+//        return "redirect:index?name="+name;
+//    }
+//
+//    private boolean isExist(String path) {
+//        File file1 = new File(path + "\\00.bmp");
+//        File file2 = new File(path + "\\64.bmp");
+//        return file1.exists() && file2.exists();
+//    }
 }
